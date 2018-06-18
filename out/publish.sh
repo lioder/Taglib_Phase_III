d207d95c50b5e5efd6d2b0dd94274f468a3bb5c6
161250042 <161250042@smail.nju.edu.cn>
2018-06-18 23:19:51 +0800

    chore: 修改部署脚本
    Former-commit-id: 200b451dde88a5b590876defea2ca88136caf070

--- out/publish.sh
+++ out/publish.sh
@@ -1,4 +1,4 @@
 mysql -u root -padmin <<EOF
     create database IF NOT EXISTS taglibdatabase charset utf8;
 EOF
-nohup java -jar taglib-0.0.1-SNAPSHOT &
+nohup java -jar taglib-0.0.1-SNAPSHOT.jar &
